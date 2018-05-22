a5fce2e6fe634ac6098acbe0e3ebf78a116d83ba
masanarih0ri <masanarihoridevelop@gmail.com>
2018-05-23 00:47:41 +0900

    42thcommit

--- sw.js
+++ sw.js
@@ -15,18 +15,6 @@ self.addEventListener('install', function(event) {
                 return cache.addAll(urlsToCache);
             })
     );
-
-    const isIos = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test( userAgent );
-    }
-    // Detects if device is in standalone mode
-    const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-    // Checks if should display install popup notification:
-    if (isIos() && !isInStandaloneMode()) {
-    this.setState({ showInstallMessage: true });
-    }
 });
 
 // リソースフェッチ時のキャッシュロード処理
