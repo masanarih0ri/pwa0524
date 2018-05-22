b7b89a6599e750ea0eca32d67acbe8850562e435
masanarih0ri <masanarihoridevelop@gmail.com>
2018-05-23 00:35:53 +0900

    40thcommit

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
+const isIos = () => {
+const userAgent = window.navigator.userAgent.toLowerCase();
+return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+this.setState({ showInstallMessage: true });
+}
+
 // キャッシュファイルの指定
 var CACHE_NAME = 'pwa0524';
 var urlsToCache = [
